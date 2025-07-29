adb1d2fe91e2fd0199c262c542b5de4923c2b83d
Ivan Volodichev <ivan_vol@icloud.com>
2025-07-29 16:38:42 +0300

    fix morning sending

--- routes/console.php
+++ routes/console.php
@@ -3,10 +3,10 @@
 use App\Console\Commands\SendMorningForecast;
 use Illuminate\Support\Facades\Schedule;
 
-// Schedule::command(SendMorningForecast::class)
-//     ->timezone('Europe/Moscow')
-//     ->dailyAt('7:00');
-
 Schedule::command(SendMorningForecast::class)
     ->timezone('Europe/Moscow')
-    ->everyMinute();
+    ->dailyAt('7:00');
+
+// Schedule::command(SendMorningForecast::class)
+//     ->timezone('Europe/Moscow')
+//     ->everyMinute();
